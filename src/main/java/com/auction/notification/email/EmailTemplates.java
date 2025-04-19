298543defd1da8b44565149a2462bdc0b5259b05
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2025-04-19 19:47:43 +0100

    for clarification.

--- src/main/java/com/auction/notification/email/EmailTemplates.java
+++ src/main/java/com/auction/notification/email/EmailTemplates.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum EmailTemplates {
     PAYMENT_CONFIRMATION("payment-confirmation.html", "Payment successfully processed"),
     ORDER_CONFIRMATION("order-confirmation.html", "Order confirmation"),
-    BID_WINNER_CONFIRMATION("bid-winner-confirmation.html", "bid confirmation");
+    BID_WINNER_CONFIRMATION("bid-winner-confirmation.html", "bid winning confirmation");
 
     private final String template;
     private final String subject;
